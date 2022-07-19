6b4208fd5a3ad46fde08583ff07defabf935d886
shatrughndev@gmail.com <shatrughndev@gmail.com>
2022-07-20 01:13:32 +0530

    Angular Changes orders & purchase

--- angular/src/app/components/orders/orders-form/orders-form.component.ts
+++ angular/src/app/components/orders/orders-form/orders-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {AbstractControl, FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {CommonService} from "../../../shared/services/common.service";
 
@@ -8,13 +8,15 @@ import {CommonService} from "../../../shared/services/common.service";
   templateUrl: './orders-form.component.html'
 })
 export class OrdersFormComponent implements OnInit {
-  form!: FormGroup;
+  // form!: FormGroup;
   mode = 'Create';
-  isSubmitted = false;
+  // isSubmitted = false;
   selectedProduct!: string;
   supplierHolder: any[] = [];
   productDDHolder: any[] = [];
   selectedSupplierId!: number;
+  ordersHolder!: any;
+  orderDetailsHolder: OrdersDetails[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<any>,
@@ -31,56 +33,67 @@ export class OrdersFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.form = this.fb.group({
-      id: [null],
-      itemCount: ['', [Validators.required]],
-      amount: ['', [Validators.required]],
-      userMasterId: [JSON.parse(localStorage.getItem('user-details')!).id],
-      isActive: [true,],
-      orderDetails: this.fb.array([])
-    });
+    // this.form = this.fb.group({
+    //   id: [null],
+    //   itemCount: ['', [Validators.required]],
+    //   amount: ['', [Validators.required]],
+    //   userMasterId: [JSON.parse(localStorage.getItem('user-details')!).id],
+    //   isActive: [true,],
+    //   orderDetails: this.fb.array([])
+    // });
 
     if (this.data) {
       console.log('Edit Data : ', this.data);
       this.mode = 'Update';
-      this.form.patchValue({
-        id: this.data.id,
-        itemCount: this.data.itemCount,
-        amount: this.data.amount,
-        userMasterId: this.data.userMasterId,
-        isActive: this.data.isActive,
-        orderDetails: this.data.orderDetails,
-      });
+      // this.form.patchValue({
+      //   id: this.data.id,
+      //   itemCount: this.data.itemCount,
+      //   amount: this.data.amount,
+      //   userMasterId: this.data.userMasterId,
+      //   isActive: this.data.isActive,
+      //   orderDetails: this.data.orderDetails,
+      // });
 
-      console.log('patchValue : ', this.form.value);
+      // console.log('patchValue : ', this.form.value);
 
     }
     this.getProduct();
   }
 
-  get orderDetailsControlArray(): FormArray {
-    return this.form.get('orderDetails') as FormArray;
-  }
+  // get orderDetailsControlArray(): FormArray {
+  //   return this.form.get('orderDetails') as FormArray;
+  // }
+  //
+  // orderDetails(): FormGroup {
+  //   return this.fb.group({
+  //     id: [null],
+  //     productMasterId: ['', [Validators.required]],
+  //     count: [0, [Validators.required]],
+  //     amount: [0, [Validators.required]],
+  //   })
+  // }
 
-  orderDetails(): FormGroup {
-    return this.fb.group({
-      id: [null],
-      productMasterId: ['', [Validators.required]],
-      count: [0, [Validators.required]],
-      amount: [0, [Validators.required]],
-    })
-  }
-
-  createOrders() {
-    this.orderDetailsControlArray.push(this.orderDetails());
-  }
-
+  // createOrders() {
+  //   this.orderDetailsControlArray.push(this.orderDetails());
+  // }
+  //
   removeOrders(i: number) {
-    this.orderDetailsControlArray.removeAt(i);
+    // this.orderDetailsControlArray.removeAt(i);
   }
 
-  get f(): { [key: string]: AbstractControl } {
-    return this.form.controls;
+  // get f(): { [key: string]: AbstractControl } {
+  //   return this.form.controls;
+  // }
+
+  addOrders(){
+    // console.log('Clicked Add Orders');
+    // let obj = new OrdersDetails();
+    // obj.id = null;
+    // obj.amount = null;
+    // obj.count = null;
+    // obj.productMasterId = null;
+    // console.log('obj : ', this.orderDetailsHolder);
+    // this.orderDetailsHolder.push(obj)
   }
 
   getProduct() {
@@ -90,41 +103,73 @@ export class OrdersFormComponent implements OnInit {
     })
   }
 
-  updateAmt(event: any, i: number){
-    let id = event.target.value;
-    console.log('id : ', id);
-    this.productDDHolder.filter((item) => {
-      if(+item.id === +id){
-        console.log('Product Price : ', this.orderDetailsControlArray.controls[i].value.amount);
-      }
-    })
+  updateAmt(event: any, i: number) {
+    // let id = event.target.value;
+    // console.log('id : ', id);
+    // this.productDDHolder.filter((item) => {
+    //   if (+item.id === +id) {
+    //     console.log('Product Price : ', this.orderDetailsControlArray.controls[i].value.amount);
+    //   }
+    // })
   }
 
   onSubmit(): void {
 
-
-    let totalCount = 0;
-    let totalAmt = 0;
-    this.form.value.orderDetails.forEach((item: any) => {
-      totalAmt = (+item.amount * item.count) + totalAmt;
-      totalCount = +item.count + totalCount;
-    });
-
-    this.form.patchValue({
-      itemCount: totalCount,
-      amount: totalAmt
-    });
-    console.log('Ta tc : ', totalAmt, totalCount);
-
-    console.log('Form Data : ', this.form.value);
-    this.isSubmitted = true;
-    if (this.form.invalid) {
-      return;
-    }
-    this.commonService.postRequest('Order/createOrUpdate', this.form.value).subscribe((resp: any) => {
-      console.log('Save Resp', resp);
-      this.dialogRef.close(true);
-    });
-
+    console.log('ordersHolder : ', this.ordersHolder);
+    //   let totalCount = 0;
+    //   let totalAmt = 0;
+    //   this.form.value.orderDetails.forEach((item: any) => {
+    //     totalAmt = (+item.amount * item.count) + totalAmt;
+    //     totalCount = +item.count + totalCount;
+    //   });
+    //
+    //   this.form.patchValue({
+    //     itemCount: totalCount,
+    //     amount: totalAmt
+    //   });
+    //   console.log('Ta tc : ', totalAmt, totalCount);
+    //
+    //   console.log('Form Data : ', this.form.value);
+    //   this.isSubmitted = true;
+    //   if (this.form.invalid) {
+    //     return;
+    //   }
+    //   this.commonService.postRequest('Order/createOrUpdate', this.form.value).subscribe((resp: any) => {
+    //     console.log('Save Resp', resp);
+    //     this.dialogRef.close(true);
+    //   });
+    //
   }
+}
+
+export class Orders implements IOrders{
+  id!: number | undefined;
+  itemCount!: number | undefined;
+  amount!: number | undefined;
+  userMasterId!: number | undefined;
+  isActive!: boolean | undefined;
+  orderDetails!: OrdersDetails[] | undefined;
+}
+
+export class OrdersDetails implements IOrdersDetails{
+  id!: number | undefined;
+  productMasterId!: number | undefined;
+  count!: number | undefined;
+  amount!: number | undefined;
+}
+
+export interface IOrders {
+  id: number | undefined;
+  itemCount: number | undefined;
+  amount: number | undefined;
+  userMasterId: number | undefined;
+  isActive: boolean | undefined;
+  orderDetails: IOrdersDetails[] | undefined;
+}
+
+export interface IOrdersDetails {
+  id: number | undefined;
+  productMasterId: number | undefined;
+  count: number | undefined;
+  amount: number | undefined;
 }
